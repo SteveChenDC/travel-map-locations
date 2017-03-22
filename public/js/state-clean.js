48cd393d8e72658e409bfc932f31b59b87f3f87b
talta <t.taltallc@gmail.com>
2017-03-21 21:17:30 -0400

    clean state file

---
+++ public/js/state-clean.js
@@ -0,0 +1,17 @@
+
+var map;
+var prev_infoWindow = false;
+
+var state = {
+	"userId":"",
+	"locations":[
+		{
+			"id": "",
+			"userId": "",
+			"address": "",
+			"latitude":"",
+			"longitude": "",
+			"notes":""
+		}
+	]
+};
